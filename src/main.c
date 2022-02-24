db7156faf79a5011fc677f1d48cf4a1ed3f1198c
Ankur Sethi <contact@ankursethi.in>
2022-02-24 13:37:56 +0530

    Create a chunk type and add basic memory operations

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
 
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
     return 0;
 }
