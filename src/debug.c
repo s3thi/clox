46c778f6d3aa592277d13ecaad586a0717295963
Ankur Sethi <contact@ankursethi.in>
2022-03-01 12:16:00 +0530

    Reformat file

--- src/debug.c
+++ src/debug.c
@@ -4,18 +4,15 @@
 #include "value.h"
 
 // `const char *` means name is a pointer to an immutable chunk of memory.
-void disassembleChunk(Chunk *chunk, const char *name)
-{
+void disassembleChunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
 
-    for (int offset = 0; offset < chunk->count;)
-    {
+    for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
     }
 }
 
-static int constantInstruction(const char *name, Chunk *chunk, int offset)
-{
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
     uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %4d '", name, constant);
     printValue(chunk->constants.values[constant]);
@@ -24,33 +21,27 @@ static int constantInstruction(const char *name, Chunk *chunk, int offset)
 }
 
 // `static` means this function is only visible within this file.
-static int simpleInstruction(const char *name, int offset)
-{
+static int simpleInstruction(const char *name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
 
-int disassembleInstruction(Chunk *chunk, int offset)
-{
+int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
-    {
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
         printf("   | ");
-    }
-    else
-    {
+    } else {
         printf("%4d ", chunk->lines[offset]);
     }
 
     uint8_t instruction = chunk->code[offset];
-    switch (instruction)
-    {
-    case OP_RETURN:
-        return simpleInstruction("OP_RETURN", offset);
-    case OP_CONSTANT:
-        return constantInstruction("OP_CONSTANT", chunk, offset);
-    default:
-        printf("Unknown opcode: %d\n", instruction);
-        return offset + 1;
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
+        default:
+            printf("Unknown opcode: %d\n", instruction);
+            return offset + 1;
     }
 }
