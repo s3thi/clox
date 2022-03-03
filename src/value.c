f2f87e0ab9727113062490befe21f458e0417393
Ankur Sethi <contact@ankursethi.in>
2022-03-03 10:23:34 +0530

    Fix potential issue while freeing ValueArray

--- src/value.c
+++ src/value.c
@@ -21,7 +21,7 @@ void writeValueArray(ValueArray *array, Value value) {
 }
 
 void freeValueArray(ValueArray *array) {
-    FREE_ARRAY(array, array->values, array->capacity);
+    FREE_ARRAY(Value, array->values, array->capacity);
     initValueArray(array);
 }
 
