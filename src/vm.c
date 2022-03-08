68164e50dc8f1f613d1f29a420a2edfd70661ac2
Ankur Sethi <contact@ankursethi.in>
2022-03-08 11:24:33 +0530

    Add all code for chapter 17

--- src/vm.c
+++ src/vm.c
@@ -75,8 +75,21 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char *source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = chunk.code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
 
 void push(Value value) {
