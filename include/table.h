c52d17649334f01ae1753e12a02c806b4a496094
Ankur Sethi <contact@ankursethi.in>
2022-03-24 15:12:48 +0530

    Add code for chapter 20

---
+++ include/table.h
@@ -0,0 +1,25 @@
+#ifndef CLOX_TABLE_H
+#define CLOX_TABLE_H
+
+#include "value.h"
+
+typedef struct {
+  ObjString *key;
+  Value value;
+} Entry;
+
+typedef struct {
+  int count;
+  int capacity;
+  Entry *entries;
+} Table;
+
+void initTable(Table *table);
+void freeTable(Table *table);
+bool tableSet(Table *table, ObjString *key, Value value);
+bool tableGet(Table *table, ObjString *key, Value *value);
+bool tableDelete(Table *table, ObjString *key);
+void tableAddAll(Table *from, Table *to);
+ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash);
+
+#endif // CLOX_TABLE_H
